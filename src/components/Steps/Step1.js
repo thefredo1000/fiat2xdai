9f964e09461cfa077a19e369bf67141ee6a57669
thefredo1000 <popobasura3@hotmail.com>
2021-05-03 17:13:04 -0500

    Step 1 Stryled

--- src/components/Steps/Step1.js
+++ src/components/Steps/Step1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import RampKeys from '../../keys/ramp-key'
-import { Button } from '@1hive/1hive-ui'
+import { Button, Text } from '@1hive/1hive-ui'
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 
 function Step1() {
@@ -10,7 +10,30 @@ function Step1() {
         padding: 5%;
       `}
     >
-      Buy xDai with Ramp
+      <h1
+        css={`
+          margin-top: 0px;
+          font-size: 30px;
+          font-weight: 700;
+          font-family: Poppins, sans-serif;
+        `}
+      >
+        Buy xDai With Fiat
+      </h1>
+      <div
+        css={`
+          max-width: 450px;
+          margin-top: 20px;
+          padding-left: 10px;
+          border-left: 4px solid #21bf73;
+        `}
+      >
+        <Text>
+          Zero Bridging Fees From Mainnet. Use the RAMP platform to skip the
+          fuss of bridging and get straight to trading.
+        </Text>
+      </div>
+      <br />
       <Button
         onClick={() => {
           new RampInstantSDK(RampKeys)
